3dee0e22cdc31334b3a337d386e4a6326d12cad9
Mohamadreza Rezaei <mohammadreza.rezaei@shab.ir>
2026-08-05 10:51:44 +0330

    :art: add project section

--- src/index.js
+++ src/index.js
@@ -117,6 +117,11 @@ export default {
             // ------------------------------------------
             "/laravel":    "https://toplearn.com/courses/web/%D8%AF%D9%88%D8%B1%D9%87-%D8%AC%D8%A7%D9%85%D8%B9-%D8%A2%D9%85%D9%88%D8%B2%D8%B4-laravel",
             "/react":      "https://toplearn.com/courses/web-design/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%B5%D9%81%D8%B1-%D8%AA%D8%A7-%D8%B5%D8%AF-reactjs",
+
+            // ------------------------------------------
+            // Featured Github Projects
+            // ------------------------------------------
+            "/gh/ai-lab-pro":     "https://github.com/mohamadreza-rezaei/ai-lab-pro",
         };
 
         const destination = redirects[path] || redirects[""];
